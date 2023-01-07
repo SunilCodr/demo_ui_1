f3f616ccf34134fa2c70afdc3d4ad86de65a5992
SunilCodr <NEYLHAM11@GMAIL.COM>
2023-01-07 14:07:50 +0530

    mobile nav bar transform translate property modified

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const closeSwitch = document.getElementById("mob-close-menu-switch")
 openSwitch.addEventListener('click', showMobilNav);
 closeSwitch.addEventListener('click', closeMobileNav);
 function showMobilNav() {
-    mobileNav.style.transform = 'translateX(-50%)';
+    mobileNav.style.transform = 'translateX(0%)';
 }
 function closeMobileNav() {
-    mobileNav.style.transform = "translateX(50%)"
+    mobileNav.style.transform = "translateX(100%)"
 }
