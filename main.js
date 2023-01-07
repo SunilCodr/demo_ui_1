a5eff70a420cc7b390c0fc4eecf11b347fac0fde
SunilCodr <NEYLHAM11@GMAIL.COM>
2023-01-07 14:21:59 +0530

    mobile nav bar transform translate property modified

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const closeSwitch = document.getElementById("mob-close-menu-switch")
 openSwitch.addEventListener('click', showMobilNav);
 closeSwitch.addEventListener('click', closeMobileNav);
 function showMobilNav() {
-    mobileNav.style.transform = 'translateX(-50%)';
+    mobileNav.style.transform = 'translateX(0%)';
+    mobileNav.setAttribute("aria-hidden", "false");
 }
 function closeMobileNav() {
-    mobileNav.style.transform = "translateX(50%)"
+    mobileNav.style.transform = "translateX(100%)"
+    mobileNav.setAttribute("aria-hidden", "true");
+
 }
