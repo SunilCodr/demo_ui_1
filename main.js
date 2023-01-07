a4ebc6069db3eef9b2efbb196c16823cd780578c
SunilCodr <NEYLHAM11@GMAIL.COM>
2023-01-07 13:22:19 +0530

    New Changes trying to solving respec error

---
+++ main.js
@@ -0,0 +1,11 @@
+const openSwitch = document.getElementById('nav-switch');
+const mobileNav = document.getElementById("mobile-nav");
+const closeSwitch = document.getElementById("mob-close-menu-switch")
+openSwitch.addEventListener('click', showMobilNav);
+closeSwitch.addEventListener('click', closeMobileNav);
+function showMobilNav() {
+    mobileNav.style.transform = 'translateX(-50%)';
+}
+function closeMobileNav() {
+    mobileNav.style.transform = "translateX(50%)"
+}
